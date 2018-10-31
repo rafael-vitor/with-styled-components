73fe585bdd24cc30615c5a9128bfae8108f3e3b2
ravi <rafael.vitor@jusbrasil.com.br>
2018-10-30 23:18:40 -0300

    Server side conditional rendering

---
+++ pages/index-b.js
@@ -0,0 +1,15 @@
+import React from 'react'
+import styled from 'styled-components'
+
+const Title = styled.h1`
+  color: red;
+  font-size: 50px;
+`
+
+export default () => {
+  console.log('TESTE B')
+  // console.log(props);
+  return (
+    <Title>Lado B</Title>
+  )
+}
